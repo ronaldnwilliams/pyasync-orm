cd765b0464da7df45b4a29ac4b529ea0f8dc4f68
Ronald Williams <ronaldnwilliams@gmail.com>
2021-05-02 11:16:53 -0500

    Add SELECT SQL to base.py with helper functions. Add tests.

--- pyasync_orm/sql/base.py
+++ pyasync_orm/sql/base.py
@@ -1,15 +1,101 @@
-from typing import List, Optional, Union
+from abc import ABC, abstractmethod
+from typing import List, Optional, Union, Tuple
 
 
-class Insert:
+LOOKUPS = {
+    'gt': '>',
+    'gte': '>=',
+    'lt': '<',
+    'lte': '<=',
+    'in': 'IN',
+    'isnull': 'ISNULL',
+}
+
+
+def pop_operator(key: str) -> Tuple[List[str], str]:
+    key_parts = key.split('__')
+    last_key_part = key_parts[len(key_parts) - 1]
+    operator = LOOKUPS.get(last_key_part)
+
+    if operator:
+        key_parts.remove(last_key_part)
+
+    return key_parts, operator or '='
+
+
+def create_where_clause(where: dict) -> str:
+    where_string = []
+    for key, value in where.items():
+        key_parts, operator = pop_operator(key)
+        # TODO handle table relationships here
+        where_string.append(f'{key_parts[0]} {operator} {value}')
+    return ' AND '.join(where_string)
+
+
+class BaseCRUDCommand(ABC):
+    def __init__(
+        self,
+        # TODO table_name might need to become Union[str, List[str]] to handle table relationships
+        table_name: str,
+        columns: Union[str, List[str]],
+    ):
+        self.table_name = table_name
+
+        if isinstance(columns, list):
+            self.columns = ", ".join(column for column in columns)
+        else:
+            self.columns = columns
+
+    @property
+    @abstractmethod
+    def sql_string(self) -> str:
+        ...
+
+
+class Select(BaseCRUDCommand):
+    def __init__(
+        self,
+        table_name: str,
+        columns: Union[str, List[str]],
+        where: Optional[dict] = None,
+        order_by: Optional[List[str]] = None,
+        limit: Optional[int] = None,
+    ):
+        super().__init__(table_name, columns)
+        if where:
+            self.where = create_where_clause(where)
+        else:
+            self.where = where
+        if order_by:
+            # TODO handle DESC
+            self.order_by = ", ".join(order for order in order_by)
+        else:
+            self.order_by = order_by
+        self.limit = limit
+
+    @property
+    def sql_string(self) -> str:
+        _sql = (
+            f'SELECT {self.columns} '
+            f'FROM {self.table_name} '
+        )
+        if self.where:
+            _sql += f'WHERE {self.where} '
+        if self.order_by:
+            _sql += f'ORDER BY {self.order_by} '
+        if self.limit:
+            _sql += f'LIMIT {self.limit}'
+        return _sql
+
+
+class Insert(BaseCRUDCommand):
     def __init__(
         self,
         table_name: str,
         columns: List[str],
         returning: Optional[Union[str, List[str]]] = None,
     ):
-        self.table_name = table_name
-        self.columns = ", ".join(column for column in columns)
+        super().__init__(table_name, columns)
         self.values = ", ".join(str(index) for index in range(1, len(columns) + 1))
         if isinstance(returning, list):
             returning = ", ".join(column for column in returning)
@@ -27,10 +113,6 @@ class Insert:
         else:
             _sql += ';'
         return _sql
-
-
-class Select:
-    pass
 
 
 class Update:
--- tests/sql/test_base.py
+++ tests/sql/test_base.py
@@ -1,4 +1,91 @@
-from pyasync_orm.sql.base import Insert
+import pytest
+
+from pyasync_orm.sql.base import Insert, pop_operator, LOOKUPS, create_where_clause, Select
+
+
+@pytest.mark.parametrize(
+    'lookup_key,lookup_value',
+    tuple((key, value) for key, value in LOOKUPS.items()),
+)
+def test_pop_operator(lookup_key, lookup_value):
+    key_part = 'foo'
+
+    key_parts, operator = pop_operator(f'{key_part}__{lookup_key}')
+
+    assert key_parts[0] == key_part
+    assert operator == lookup_value
+
+
+def test_pop_operator_none():
+    key_part = 'foo'
+
+    key_parts, operator = pop_operator(key_part)
+
+    assert key_parts[0] == key_part
+    assert operator == '='
+
+
+@pytest.mark.parametrize(
+    'where_dict,where_sql',
+    (
+        ({'foo': 1}, 'foo = 1'),
+        ({'foo__gt': 2}, 'foo > 2'),
+        ({'foo': 1, 'foo__gt': 2}, 'foo = 1 AND foo > 2'),
+    )
+)
+def test_create_where_clause(where_dict, where_sql):
+    where_string = create_where_clause(where_dict)
+
+    assert where_string == where_sql
+
+
+class TestSelect:
+    def test_init(self):
+        select = Select(
+            table_name='foo',
+            columns='*',
+        )
+
+        assert select.where is None
+        assert select.order_by is None
+        assert select.limit is None
+
+    def test_init_with_optionals(self):
+        select = Select(
+            table_name='foo',
+            columns=['a', 'b'],
+            where={'a': 1},
+            order_by=['b', 'a'],
+            limit=1,
+        )
+
+        assert select.where == 'a = 1'
+        assert select.order_by == 'b, a'
+        assert select.limit == 1
+
+    def test_sql_string(self):
+        select = Select(
+            table_name='foo',
+            columns='*',
+        )
+
+        assert select.sql_string == 'SELECT * FROM foo '
+
+    def test_sql_string_with_optionals(self):
+        select = Select(
+            table_name='foo',
+            columns=['a', 'b'],
+            where={'a': 1},
+            order_by=['b', 'a'],
+            limit=1,
+        )
+
+        assert select.sql_string == (
+            'SELECT a, b FROM foo '
+            'WHERE a = 1 '
+            'ORDER BY b, a '
+            'LIMIT 1'
+        )
 
 
 class TestInsert:
