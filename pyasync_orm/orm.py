47202001b55d225868705688a7a6ce78f25859e5
Ronald Williams <ronaldnwilliams@gmail.com>
2021-07-31 16:34:10 -0500

    Add type hints.

--- pyasync_orm/orm.py
+++ pyasync_orm/orm.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Type, Optional
+from typing import TYPE_CHECKING, Type, Optional, List
 
 from pyasync_orm.database import Database
 from pyasync_orm.sql import SQL
@@ -10,14 +10,18 @@ if TYPE_CHECKING:
 class ORM:
     database = Database()
 
-    def __init__(self, model_class: Type['Model'], sql: Optional[SQL] = None):
+    def __init__(
+        self,
+        model_class: Type['Model'],
+        sql: Optional[SQL] = None,
+    ):
         self._model_class = model_class
         self._sql = sql
 
     def _get_orm(self):
         return ORM(self._model_class, sql=SQL()) if self._sql is None else self
 
-    async def create(self, **kwargs):
+    async def create(self, **kwargs) -> 'Model':
         sql = SQL.build_insert(
             table_name=self._model_class.table_name,
             column_list=list(kwargs.keys()),
@@ -26,17 +30,17 @@ class ORM:
             results = await connection.fetch(sql, *kwargs.values())
         return self._model_class.from_db(results[0])
 
-    def filter(self, **kwargs):
+    def filter(self, **kwargs) -> 'ORM':
         orm = self._get_orm()
         orm._sql.add_where(kwargs)
         return orm
 
-    def exclude(self, **kwargs):
+    def exclude(self, **kwargs) -> 'ORM':
         orm = self._get_orm()
         orm._sql.add_where_not(kwargs)
         return orm
 
-    async def get(self, **kwargs):
+    async def get(self, **kwargs) -> 'Model':
         orm = self._get_orm()
         orm._sql.add_where(kwargs)
         sql, values = orm._sql.build_select(table_name=self._model_class.table_name)
@@ -46,21 +50,21 @@ class ORM:
             raise ValueError(f'{self._model_class.__name__} get query found more than one record.')
         return self._model_class.from_db(results[0])
 
-    async def all(self):
+    async def all(self) -> List['Model']:
         orm = self._get_orm()
         sql, values = orm._sql.build_select(table_name=self._model_class.table_name)
         async with self.database.get_connection() as connection:
             results = await connection.fetch(sql, *values)
         return [self._model_class.from_db(result) for result in results]
 
-    async def update(self):
+    async def update(self) -> List['Model']:
         orm = self._get_orm()
         sql, values = orm._sql.build_update(table_name=self._model_class.table_name)
         async with self.database.get_connection() as connection:
             results = await connection.fetch(sql, *values)
         return [self._model_class.from_db(result) for result in results]
 
-    async def delete(self):
+    async def delete(self) -> List['Model']:
         orm = self._get_orm()
         sql, values = orm._sql.build_delete(table_name=self._model_class.table_name)
         async with self.database.get_connection() as connection:
