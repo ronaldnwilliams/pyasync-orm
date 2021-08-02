7cbcae6af77ba4fcc99184ccbe64596d36fc7b43
Ronald Williams <ronaldnwilliams@gmail.com>
2021-08-02 09:16:56 -0500

    Outline fields.

--- pyasync_orm/fields.py
+++ pyasync_orm/fields.py
@@ -1,24 +1,112 @@
+import json
+from typing import Optional, Type, Union, Any, Callable
+
+
 class BaseField:
     def __init__(
         self,
+        null: bool = True,
+        default: Union[Any, Callable[[], Any]] = None,
         primary_key: bool = False,
+        unique: bool = False,
     ):
+        self.null = null
+        self.default = default
         self.primary_key = primary_key
+        self.unique = unique
 
 
-class SmallSerial(BaseField):
+class SmallSerialField(BaseField):
     pass
 
 
-class Serial(BaseField):
+class SerialField(BaseField):
     pass
 
 
-class BigSerial(BaseField):
+class BigSerialField(BaseField):
     pass
 
 
-class VarChar(BaseField):
-    def __init__(self, max_length: int, **kwargs):
+class SmallIntegerField(BaseField):
+    pass
+
+
+class IntegerField(BaseField):
+    pass
+
+
+class BigIntegerField(BaseField):
+    pass
+
+
+class PositiveSmallIntegerField(BaseField):
+    pass
+
+
+class PositiveIntegerField(BaseField):
+    pass
+
+
+class PositiveBigIntegerField(BaseField):
+    pass
+
+
+class DecimalField(BaseField):
+    def __init__(
+        self,
+        max_digits: int,
+        decimal_places: int,
+        **kwargs,
+    ):
+        self.max_digits = max_digits
+        self.decimal_places = decimal_places
+        super().__init__(**kwargs)
+
+
+class FloatField(BaseField):
+    pass
+
+
+class VarCharField(BaseField):
+    def __init__(
+        self,
+        max_length: int,
+        **kwargs,
+    ):
         self.max_length = max_length
         super().__init__(**kwargs)
+
+
+class TextField(BaseField):
+    pass
+
+
+class JSONField(BaseField):
+    def __init__(
+        self,
+        encoder: Optional[Type[json.JSONEncoder]] = None,
+        decoder: Optional[Type[json.JSONDecoder]] = None,
+        **kwargs,
+    ):
+        self.encoder = encoder or json.JSONEncoder
+        self.decoder = decoder or json.JSONDecoder
+        super().__init__(**kwargs)
+
+
+class DateField(BaseField):
+    def __init__(
+        self,
+        auto_now: bool = False,
+        auto_now_add: bool = False,
+        **kwargs,
+    ):
+        if auto_now and auto_now_add:
+            raise ValueError('DateField error; auto_now and auto_now_add. Only set one of these to True')
+        self.auto_now = auto_now
+        self.auto_now_add = auto_now_add
+        super().__init__(**kwargs)
+
+
+class DateTimeField(DateField):
+    pass
--- pyasync_orm/models.py
+++ pyasync_orm/models.py
@@ -1,6 +1,6 @@
 import inflection
 
-from pyasync_orm.fields import BigSerial
+from pyasync_orm.fields import BigSerialField
 from pyasync_orm.orm import ORM
 
 
@@ -11,7 +11,7 @@ class Model:
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls.table_name = inflection.tableize(cls.__name__)
-        cls.id = BigSerial(primary_key=True)
+        cls.id = BigSerialField(primary_key=True)
         cls.orm = ORM(model_class=cls)
 
     def __str__(self):
